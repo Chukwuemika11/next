778c27771db0558114dd7b21152099da406191b2
Chukwuemika11 <117942512+Chukwuemika11@users.noreply.github.com>
2024-04-02 23:09:00 +0100

    Add files via upload

---
+++ Links.jsx
@@ -0,0 +1,36 @@
+import React from 'react'
+import styles from "./links.module.css"
+
+const Links = () => {
+ 
+    const links = [
+        {
+          title :"Homepage",
+          path: "/",
+        },
+
+        {
+            title:"About",
+            path: "/about"
+          },
+
+          {
+            title:"Contact",
+            path: "/contact"
+          },
+
+          {
+            title:"Blog",
+            path: "/blog"
+          }
+        ]
+  return(
+    <div className={styles.links}>
+        {links.map((link =>{
+      <navLink item={link} key={link.title}/>
+        }))}
+    </div>
+  )
+}
+
+export default Links
